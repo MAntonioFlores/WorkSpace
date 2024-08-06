540233b1e7aa5e82892425b2b470968937c89507
MAntonioFlores <zerofenix.wrath@gmail.com>
2024-08-06 17:36:26 -0600

    Update Palindromo Expresion regular

--- JavaScript/Palindromo.js
+++ JavaScript/Palindromo.js
@@ -1,8 +1,12 @@
 const palindromo = (palabra) => {
-    palabra = palabra.replace(' ', '');
+
+    palabra = palabra.replace(/\s+/g, '');///\s+/g es una expresion regular la cual quita todos los espacios en blanco y la es que buscara globalmentge y no se quedara con el primero que encuentre.
+    console.log(palabra);
     palabra = palabra.toLowerCase();
+    
 
     let palabraReversa = palabra.split('').reverse().join('');
+
 
     if (palabra === palabraReversa) {
         return true;
@@ -11,4 +15,14 @@ const palindromo = (palabra) => {
     }
 }
 
-console.log(palindromo('Anita Lava La Tina'));
+// console.log(palindromo('Anita Lava La Tina'));
+let palabra = prompt('Ingrese una palabra');
+
+let esPalindromo = palindromo(palabra);
+
+if (esPalindromo) {
+    document.write('Es Palindromo');
+
+}else{
+    document.write('No es Palindromo');
+}
